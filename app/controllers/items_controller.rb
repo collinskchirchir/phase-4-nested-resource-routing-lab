633deecef53eecc4091ab540492cf981c6f3e247
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-25 11:20:33 +0300

    users-userid-index method done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,22 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+
+  private
+
+  def render_not_found_response
+    render json: {error: "User not found"}, status: :not_found
+  end
+
 
 end
