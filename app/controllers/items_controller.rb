3d123639f4e5900cfb5cd4446cef6282efe8d7d3
Collins Chirchir <collinskchirchir@gmail.com>
2023-03-25 11:33:37 +0300

    user-userid-items create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,7 +21,21 @@ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     render json: item, status: :ok
   end
 
+  def create
+    if params[:user_id]
+      user = User.find_by!(id: params[:user_id])
+      item = user.items.create!(items_params)
+    else
+      item = Item.find_by!(id: params[:id])
+    end
+    render json: item, status: :created
+  end
+
   private
+
+  def items_params
+    params.permit(:name, :description, :price, :user_id)
+  end
 
   def render_not_found_response
     render json: {error: "User not found"}, status: :not_found
